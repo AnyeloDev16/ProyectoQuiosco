8fa96ee9ed6609f89c1360b6c38930e424821a68
Anyelo Nuñez Obispo <anyelonuob2004@gmail.com>
2024-06-07 21:43:49 -0500

    primer commit

---
+++ src/main/main.java
@@ -0,0 +1,15 @@
+package main;
+
+import vista.Login;
+
+public class main {
+
+    public static void main(String[] args) {
+        
+        Login mv = new Login();
+        
+        mv.setVisible(true);
+        
+    }
+
+}
---
+++ src/vista/Login.java
@@ -0,0 +1,40 @@
+package vista;
+
+import java.awt.Color;
+
+public class Login extends javax.swing.JFrame {
+
+    public Login() {
+        initComponents();
+        this.setLocationRelativeTo(null);
+        this.setTitle("Login - Quiosco");
+        this.setResizable(false);
+        this.setSize(800, 500);
+        this.setBackground(Color.yellow);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 800, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 500, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
+
+
